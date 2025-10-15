e5b0f2bd0a35a5bbd91a75dac1cb3772ed5b4406
kenzosashida090 <kenzosashida1@gmail.com>
2025-10-15 11:37:58 -0600

    [new] Conditional if

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test:{
+        reporters: ['verbose', 'json'],
         environment:'jsdom',// indicate the server node that we using dom element with this library
         globals:true, // avoid to import basics testing-library/react like (it)
         setupFiles:[ 'test/setup.ts'], //avoid importing library import "@testing-library/jest-dom/vitest" in all test files
+        outputFile: {
+            junit: './junit-report.xml',
+            json: './json-report.json',
+    },
     }
 })
